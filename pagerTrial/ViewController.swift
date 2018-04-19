c4e880c45b77662f886ecdd3047bdf75d7273865
ios <shpuj129@gmail.com>
2018-04-19 14:54:29 +0530

    Initial Commit

---
+++ pagerTrial/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pagerTrial
+//
+//  Created by ios1 on 19/04/18.
+//  Copyright © 2018 ios1. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
